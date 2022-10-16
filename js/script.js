421b822436c62268d5c6643286f30b15bcdca3d6
kevinrimbun03 <kevinrimbun03>
2022-10-16 20:35:27 +0700

    check bug

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+function validate() {
+    var email = document.forms["loginForm"]["email"].value;
+    var pass = document.forms["loginForm"]["password"].value;
+    if (email === "niki@mail.com" && pass === "niki123") {
+        alert("Welcome Niki");
+        // localStorage.setItem("auth", 1)
+        window.location.href = "home.html"
+    }
+    else {
+        alert("Please enter the valid Email & Password")
+    }
+}
 
+let changeIcon = function(icon) {
+    icon.classList.toggle('bi-x')
+}
+
+function borrow() {
+    let transform = document.getElementById('borrow')
+    transform.style.backgroundColor = 'rgba(153, 216, 21, 1)'
+    transform.innerHTML = 'Borrowed'
+}
